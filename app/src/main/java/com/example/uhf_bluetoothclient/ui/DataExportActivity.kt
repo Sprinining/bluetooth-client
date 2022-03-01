44839fb092a82141b6f752558ab6d8faa40997f7
周浩楠 <zhn2174@gmail.com>
2022-03-01 19:15:20 +0800

    新增安装信息上报(未完成)

--- app/src/main/java/com/example/uhf_bluetoothclient/ui/DataExportActivity.kt
+++ app/src/main/java/com/example/uhf_bluetoothclient/ui/DataExportActivity.kt
@@ -8,8 +8,11 @@ import com.example.uhf_bluetoothclient.entity.CityBean
 import com.example.uhf_bluetoothclient.entity.ProvinceBean
 import com.example.uhf_bluetoothclient.viewmodel.DataExportModel
 import com.lxj.xpopup.XPopup
+import com.seuic.util.common.SPUtils
 import com.seuic.util.common.ext.getString
+import com.seuic.util.common.ext.singleClick
 import com.seuic.util.common.ext.toTypeClassList
+import com.seuic.util.common.ext.toastShort
 
 /**
  *
@@ -51,6 +54,111 @@ class DataExportActivity : BaseActivity<ActivityDataExportBinding, DataExportMod
                     .setOnSelectListener { _, data ->
                         binding.exportVm!!.lastCity.value = data.name
                     }).show()
+        }
+        binding.exportBranchesEt.setDrawableRightClickListener {
+            val exportBranches =
+                SPUtils.getInstance().getString("exportBranches", null).toTypeClassList<String>()
+                    ?.toMutableList() ?: mutableListOf()
+            exportBranches.add("清除网点记录！")
+            XPopup.Builder(this).atView(binding.exportBranchesEt)
+                .asAttachList(exportBranches.toTypedArray(), null) { pos, text ->
+                    if (exportBranches.size > 1 && pos == exportBranches.size - 1) {
+                        XPopup.Builder(this).asConfirm("提示", "请确认是否清除网点记录！", "取消", "清除", {
+                            SPUtils.getInstance().remove("exportBranches")
+                        }, null, false).show()
+                        return@asAttachList
+                    }
+                    binding.exportBranchesEt.setText(text)
+                }.show()
+        }
+        val locations = arrayOf("仓库", "正门", "后门", "楼梯", "食堂", "ATM", "二楼门")
+        binding.exportLocationEt.setDrawableRightClickListener {
+            XPopup.Builder(this).atView(binding.exportLocationEt)
+                .asAttachList(locations, null) { _, text ->
+                    binding.exportLocationEt.setText(text)
+                }.show()
+        }
+
+        binding.connectBtn.singleClick {
+            // FIXME: 蓝牙连接设备
+        }
+        binding.updateBtn.singleClick {
+            // FIXME: 获取数据
+        }
+        val ants = arrayOf("1", "2", "3", "4", "5", "6", "7", "8")
+        binding.exportAntTv.singleClick {
+            XPopup.Builder(this).atView(binding.exportAntTv)
+                .asAttachList(ants, null) { _, text ->
+                    binding.exportAntTv.setText(text)
+                }.show()
+        }
+        val powerTypes = arrayOf("电源", "POE")
+        binding.exportPowerTypeTv.singleClick {
+            XPopup.Builder(this).atView(binding.exportPowerTypeTv)
+                .asAttachList(powerTypes, null) { _, text ->
+                    binding.exportPowerTypeTv.setText(text)
+                }.show()
+        }
+        val useForTypes = arrayOf("盘点", "门禁")
+        binding.exportUseForEt.setDrawableRightClickListener {
+            XPopup.Builder(this).atView(binding.exportUseForEt)
+                .asAttachList(useForTypes, null) { _, text ->
+                    binding.exportUseForEt.setText(text)
+                }.show()
+        }
+
+        binding.toolbarMoreTv.singleClick {
+            if (binding.exportProvinceEt.length()<=0){
+                toastShort { "请输入省！" }
+                return@singleClick
+            }
+            if (binding.exportCityEt.length()<=0){
+                toastShort { "请输入城市！" }
+                return@singleClick
+            }
+            if (binding.exportBranchesEt.length()<=0){
+                toastShort { "请输入网点！" }
+                return@singleClick
+            }
+            if (binding.exportLocationEt.length()<=0){
+                toastShort { "请输入安装位置！" }
+                return@singleClick
+            }
+            if (binding.exportAddressEt.length()<=0){
+                toastShort { "请输入详细信息！" }
+                return@singleClick
+            }
+            if (binding.exportSnEt.length()<=0){
+                toastShort { "请输入设备SN！" }
+                return@singleClick
+            }
+            if (binding.exportIpv4Et.length()<=0){
+                toastShort { "请输入IPV4！" }
+                return@singleClick
+            }
+            if (binding.exportIpv4InfoEt.length()<=0){
+                toastShort { "请输入IPV4网端信息！" }
+                return@singleClick
+            }
+            if (binding.exportIpv6Et.length()<=0){
+                toastShort { "请输入IPV6！" }
+                return@singleClick
+            }
+            if (binding.exportImeiEt.length()<=0){
+                toastShort { "请输入IMEI！" }
+                return@singleClick
+            }
+            if (binding.exportRj45macEt.length()<=0){
+                toastShort { "请输入RJ45MAC地址！" }
+                return@singleClick
+            }
+            if (binding.exportIpv4InfoEt.length()<=0){
+                toastShort { "请输入IPV4网端信息！" }
+                return@singleClick
+            }
+            // FIXME: 以上校验完成。。。。。。
+
+
         }
     }
 
